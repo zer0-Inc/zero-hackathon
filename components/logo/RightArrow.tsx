6f6c4b6de31db923b5500c9f8982b821177120c5
No.366 <woongcheol99@naver.com>
2024-08-13 22:15:39 +0900

    avatar,right_arrow logo add

---
+++ components/logo/RightArrow.tsx
@@ -0,0 +1,37 @@
+export default function RightArrow() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask
+        id="mask0_122_1525"
+        maskUnits="userSpaceOnUse"
+        x="2"
+        y="0"
+        width="8"
+        height="12"
+        style={{ maskType: "alpha" }}
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M2.72592 0.469661C2.4249 0.762543 2.4249 1.2374 2.72592 1.53028L7.31967 5.99978L2.72592 10.4693C2.4249 10.7622 2.4249 11.237 2.72592 11.5299C3.02694 11.8228 3.515 11.8228 3.81602 11.5299L9.49988 5.99978L3.81602 0.469661C3.515 0.17678 3.02694 0.17678 2.72592 0.469661Z"
+          fill="#006FFD"
+        />
+      </mask>
+      <g mask="url(#mask0_122_1525)">
+        <rect
+          x="0.000244141"
+          y="-0.000976562"
+          width="11.9995"
+          height="11.9995"
+          fill="#8F9098"
+        />
+      </g>
+    </svg>
+  );
+}
